edeb7cd32d68489ebf1554c7d3de65fd51fe34d5
VasDA <D.Vasilyev@bssys.com>
2016-07-21 20:03:53 +0300

    Immutable http://howtodoinjava.com/core-java/related-concepts/how-to-make-a-java-class-immutable/

---
+++ src/main/java/immutebleClass/TestMain.java
@@ -0,0 +1,24 @@
+package immutebleClass;
+
+import java.util.Date;
+
+/**
+ * Created by VasDA on 21.07.2016.
+ */
+class TestMain
+{
+    public static void main(String[] args)
+    {
+        ImmutableClass im = ImmutableClass.createNewInstance(100,"test", new Date());
+        System.out.println(im);
+        tryModification(im.getImmutableField1(),im.getImmutableField2(),im.getMutableField());
+        System.out.println(im);
+    }
+
+    private static void tryModification(Integer immutableField1, String immutableField2, Date mutableField)
+    {
+        immutableField1 = 10000;
+        immutableField2 = "test changed";
+        mutableField.setDate(10);
+    }
+}
