f490d3a011e2ae03bcbdbdbdb356c81720713b5b
VasDA <D.Vasilyev@bssys.com>
2016-07-21 17:19:13 +0300

    Epam's lectures. One

---
+++ src/main/java/lecture/one/ListenerTest.java
@@ -0,0 +1,12 @@
+package lecture.one;
+
+import javax.swing.*;
+
+/**
+ * Created by VasDA on 21.07.2016.
+ */
+public class ListenerTest {
+    public ListenerTest() {
+        JButton jButton = new JButton();
+    }
+}
