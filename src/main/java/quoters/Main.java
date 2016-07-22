7b82c739ccb79d5900c3ec89a67e6c0628d61d78
VasDA <D.Vasilyev@bssys.com>
2016-07-22 18:06:41 +0300

    EpamSpring TalkingRobot

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -11,22 +11,22 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("application-context.xml");
-        ShakeSpearQuoter bean = classPathXmlApplicationContext.getBean(ShakeSpearQuoter.class);
-        bean.sayQuote();
-
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        Quoter bean2 = classPathXmlApplicationContext.getBean("terminatorQuoter", TerminatorQuoter.class);
-        bean2.sayQuote();
-
-        System.out.println("");
-        System.out.println("");
-        Map<String, Quoter> beansOfType = classPathXmlApplicationContext.getBeansOfType(Quoter.class);
-        Collection<Quoter> quoters = beansOfType.values();
-        for (Quoter quoter : quoters) {
-            quoter.sayQuote();
-        }
+//        ShakeSpearQuoter bean = classPathXmlApplicationContext.getBean(ShakeSpearQuoter.class);
+//        bean.sayQuote();
+//
+//        System.out.println("");
+//        System.out.println("");
+//        System.out.println("");
+//        Quoter bean2 = classPathXmlApplicationContext.getBean("terminatorQuoter", TerminatorQuoter.class);
+//        bean2.sayQuote();
+//
+//        System.out.println("");
+//        System.out.println("");
+//        Map<String, Quoter> beansOfType = classPathXmlApplicationContext.getBeansOfType(Quoter.class);
+//        Collection<Quoter> quoters = beansOfType.values();
+//        for (Quoter quoter : quoters) {
+//            quoter.sayQuote();
+//        }
 
     }
 }
---
+++ src/main/java/quoters/TalkingRobot.java
@@ -0,0 +1,8 @@
+package quoters;
+
+/**
+ * Created by VasDA on 22.07.2016.
+ */
+public interface TalkingRobot {
+    void talk();
+}
