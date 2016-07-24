39352bfdee2adf290ee307de3ea0fa0090d81879
Dmitry <dimysik@gmail.com>
2016-07-24 18:43:54 +0300

    array copy Изменения, чтобы лучше понять как проиходит копирование ссылок в arraycopy.

--- src/main/java/colvirTests/Test.java
+++ src/main/java/colvirTests/Test.java
@@ -5,12 +5,18 @@ package colvirTests;
  */
 public class Test {
     public static void main(String[] args) {
-        int[][] x = new int[2][5];
-        int[][] y = new int[2][5];
+        int[][] x = {{11,12,13,14,15},{21,22,23,24,25}};
+        int[][] y = {{111,112,113,114,115},{221,222,223,224,225}};
 
         System.out.println(x[1][3]);
-        System.arraycopy(x, 0, y, 0, x.length);
-        x[1][3] = 55;
-        System.out.println(y[1][3]);
+        System.arraycopy(x, 0, y, 0, 1);
+        x[0][3] = 55;
+        for (int[] ints : y) {
+            for (int anInt : ints) {
+                System.out.print(anInt +  ", ");
+            }
+            System.out.println();
+        }
+        System.out.println(y[0][3]);
     }
 }
