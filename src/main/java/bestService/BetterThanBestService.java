892d15648c3484f35fa8a62dce823337cc25f315
VasDA <D.Vasilyev@bssys.com>
2016-07-25 17:42:38 +0300

    EpamSpring BestService

---
+++ src/main/java/bestService/BetterThanBestService.java
@@ -0,0 +1,16 @@
+package bestService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by VasDA on 25.07.2016.
+ */
+public class BetterThanBestService extends BestService {
+    private List cache = new ArrayList();
+
+    @Override
+    public void chuckNorrisMethod() {
+        cache.add(1);
+    }
+}
