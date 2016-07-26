76a798153514fe01fa783fb258e1c47d6be33d48
Dmitry <dimysik@gmail.com>
2016-07-26 03:52:14 +0300

    bad

--- src/main/java/evBorisov/epamSpring/fraemwork/Benchmark.java
+++ src/main/java/evBorisov/epamSpring/fraemwork/Benchmark.java
@@ -1,7 +1,11 @@
 package evBorisov.epamSpring.fraemwork;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Created by Dmitry on 26.07.2016.
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Benchmark {
 }
