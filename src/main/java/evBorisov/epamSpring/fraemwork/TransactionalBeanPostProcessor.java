b3924b72cc414ef645465a3adc562b64c3666a56
VasDA <D.Vasilyev@bssys.com>
2016-07-26 18:29:39 +0300

    EpamSpring Transactional решение проблемы вызова методов инициализации на примере Transactional. Решение проблемы на Benchmark будет реализовано вторым способом

--- src/main/java/evBorisov/epamSpring/fraemwork/TransactionalBeanPostProcessor.java
+++ src/main/java/evBorisov/epamSpring/fraemwork/TransactionalBeanPostProcessor.java
@@ -6,19 +6,29 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by VasDA on 26.07.2016.
  */
 public class TransactionalBeanPostProcessor implements BeanPostProcessor {
+
+    private Map<String, Class> map = new HashMap<String, Class>();
+
     public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
+        Class<?> originalClass = o.getClass();
+        if (originalClass.isAnnotationPresent(Transactional.class)) {
+            map.put(s, originalClass);
+        }
         return o;
     }
 
     public Object postProcessAfterInitialization(final Object o, String s) throws BeansException {
         Class clazz = o.getClass();
 
-        if (clazz.isAnnotationPresent(Transactional.class)) {
+        Class originalClass = map.get(s);
+        if (originalClass != null) {
             return Proxy.newProxyInstance(clazz.getClassLoader(), clazz.getInterfaces(), new InvocationHandler() {
                 public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
                     System.out.println("Transaction opened");
