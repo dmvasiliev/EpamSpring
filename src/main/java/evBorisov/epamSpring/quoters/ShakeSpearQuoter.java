c4984924443e78756599e54e42f232efd791f303
VasDA <D.Vasilyev@bssys.com>
2016-07-26 17:24:34 +0300

    EpamSpring Benchmark над методом Версия Борисова

--- src/main/java/evBorisov/epamSpring/quoters/ShakeSpearQuoter.java
+++ src/main/java/evBorisov/epamSpring/quoters/ShakeSpearQuoter.java
@@ -1,7 +1,5 @@
 package evBorisov.epamSpring.quoters;
 
-import evBorisov.epamSpring.fraemwork.Benchmark;
-
 /**
  * Created by Dmitry on 22.07.2016.
  */
@@ -9,7 +7,6 @@ public class ShakeSpearQuoter implements Quoter {
 
     private String message;
 
-    @Benchmark
     public void sayQuote() {
         System.out.println(message);
     }
