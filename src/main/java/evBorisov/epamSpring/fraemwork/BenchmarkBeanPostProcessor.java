935da35b70c9aa91d13f82c4cbcc1c025911a2ab
VasDA <D.Vasilyev@bssys.com>
2016-07-26 18:49:20 +0300

    EpamSpring Transactional второй способ

--- src/main/java/evBorisov/epamSpring/fraemwork/BenchmarkBeanPostProcessor.java
+++ src/main/java/evBorisov/epamSpring/fraemwork/BenchmarkBeanPostProcessor.java
@@ -1,7 +1,10 @@
 package evBorisov.epamSpring.fraemwork;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
@@ -11,38 +14,52 @@ import java.lang.reflect.Proxy;
  * Created by Dmitry on 26.07.2016.
  */
 public class BenchmarkBeanPostProcessor implements BeanPostProcessor {
+
+    @Autowired
+    private ConfigurableListableBeanFactory factory;
+
     public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
         return o;
     }
 
     public Object postProcessAfterInitialization(final Object o, String s) throws BeansException {
-        Class clazz = o.getClass();
-        Method[] methods = clazz.getMethods();
-        boolean benchmarkFound = false;
-        for (Method method : methods) {
-            if (method.isAnnotationPresent(Benchmark.class)) {
-                benchmarkFound = true;
-                break;
-            }
+        BeanDefinition beanDefinition = factory.getBeanDefinition(s);
+        String beanClassName = beanDefinition.getBeanClassName();
+        Class<?> originalClass = null;
+        try {
+            originalClass = Class.forName(beanClassName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
-
-        if (benchmarkFound) {
-            Object proxy = Proxy.newProxyInstance(clazz.getClassLoader(), clazz.getInterfaces(), new InvocationHandler() {
-
-                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                    Method originalClassMethod = o.getClass().getMethod(method.getName(), method.getParameterTypes());
-                    if (originalClassMethod.isAnnotationPresent(Benchmark.class)) {
-                        long before = System.nanoTime();
-                        Object retVal = method.invoke(o, args);
-                        long after = System.nanoTime();
-                        System.out.println(after - before);
-                        return retVal;
-                    } else {
-                        return method.invoke(o, args);
-                    }
+        if (originalClass != null) {
+            Method[] methods = originalClass.getMethods();
+            boolean benchmarkFound = false;
+            for (Method method : methods) {
+                if (method.isAnnotationPresent(Benchmark.class)) {
+                    benchmarkFound = true;
+                    break;
                 }
-            });
-            return proxy;
+            }
+
+            if (benchmarkFound) {
+                final Class<?> finalOriginalClass = originalClass;
+                Object proxy = Proxy.newProxyInstance(originalClass.getClassLoader(), originalClass.getInterfaces(), new InvocationHandler() {
+
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        Method originalClassMethod = finalOriginalClass.getMethod(method.getName(), method.getParameterTypes());
+                        if (originalClassMethod.isAnnotationPresent(Benchmark.class)) {
+                            long before = System.nanoTime();
+                            Object retVal = method.invoke(o, args);
+                            long after = System.nanoTime();
+                            System.out.println(after - before);
+                            return retVal;
+                        } else {
+                            return method.invoke(o, args);
+                        }
+                    }
+                });
+                return proxy;
+            }
         }
         return o;
     }
