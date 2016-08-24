d6669b67d3e9e33e3f7588320af27c4a9ece382d
VasDA <D.Vasilyev@bssys.com>
2016-08-24 13:21:13 +0300

    FirstGroovy

---
+++ src/main/groovy/FirstGroovy.groovy
@@ -0,0 +1,11 @@
+/**
+ * Created by VasDA on 24.08.2016.
+ */
+class FirstGroovy {
+    public static void main(String[] args) {
+        println ""
+        print "Hello Groovy"
+        println ""
+    }
+
+}
