a042c83c292c34c047add9f6712da634770c9cd5
VasDA <D.Vasilyev@bssys.com>
2016-09-15 16:40:31 +0300

    final in method

--- src/main/java/evBorisov/epamJava/one/ListenerTest.java
+++ src/main/java/evBorisov/epamJava/one/ListenerTest.java
@@ -1,12 +1,23 @@
 package evBorisov.epamJava.one;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by VasDA on 21.07.2016.
  */
 public class ListenerTest {
     public ListenerTest() {
+        final int x = 1;
+
         JButton jButton = new JButton();
+        jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(x);
+                System.err.println(x);
+            }
+        });
     }
 }
