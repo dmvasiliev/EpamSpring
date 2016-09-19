26fe57e326454c738b620e6df06338318a3075a6
Dmitry <dimysik@gmail.com>
2016-09-20 01:03:56 +0300

    Benchmark

--- src/main/java/evBorisov/epamJava/four/Benchmark.java
+++ src/main/java/evBorisov/epamJava/four/Benchmark.java
@@ -7,6 +7,8 @@ package evBorisov.epamJava.four;
 public class Benchmark {
 
     public static void main(String[] args) {
+//        double sqrt1 = Math.sqrt(4);
+
         long before = System.nanoTime();
         int rep = 10000000;
         double sqrt = 0;
@@ -15,6 +17,7 @@ public class Benchmark {
         }
         long after = System.nanoTime();
         System.out.println(sqrt);
-        System.out.println(after - before);
+//        System.out.println(sqrt1);
+        System.out.println((after - before) / rep);
     }
 }
