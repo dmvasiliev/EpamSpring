d470820e81db9b51968b7dd91c309178393bef8e
VasDA <D.Vasilyev@bssys.com>
2016-09-19 17:22:56 +0300

    My class loader

--- src/main/java/evBorisov/epamJava/three/MyClassLoader.java
+++ src/main/java/evBorisov/epamJava/three/MyClassLoader.java
@@ -13,12 +13,19 @@ public class MyClassLoader extends ClassLoader {
 
     @Override
     public Class<?> loadClass(String name) throws ClassNotFoundException {
-        System.out.println("print time before load class: " + System.currentTimeMillis());
+        System.out.println("print time before load class: ");
+        System.out.println(name);
         return super.loadClass(name);
     }
 
+    //ToDo -Djava.system.class.loader=evBorisov.epamJava.three.MyClassLoader
     public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-        MyClassLoader myClassLoader = new MyClassLoader();
-        myClassLoader.loadClass("evBorisov.epamJava.three.Person").newInstance();
+//        MyClassLoader myClassLoader = new MyClassLoader();
+//        myClassLoader.loadClass("evBorisov.epamJava.three.Person").newInstance();
+        new Person();
+    }
+
+    public MyClassLoader(ClassLoader parent) {
+        super(parent);
     }
 }
