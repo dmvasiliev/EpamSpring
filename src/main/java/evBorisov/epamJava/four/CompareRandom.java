948bbfb9d042c713c41d2b97308cf5b32441bc44
VasDA <D.Vasilyev@bssys.com>
2016-09-20 15:10:20 +0300

    benchmark .hotspot_compiler

---
+++ src/main/java/evBorisov/epamJava/four/CompareRandom.java
@@ -0,0 +1,35 @@
+package evBorisov.epamJava.four;
+
+import java.lang.management.CompilationMXBean;
+import java.lang.management.ManagementFactory;
+
+/**
+ * Created by VasDA on 20.09.2016.
+ */
+public class CompareRandom {
+    public static void main(String[] args) {
+        CompilationMXBean mxBean = ManagementFactory.getCompilationMXBean();
+        long totalCompilationTime = mxBean.getTotalCompilationTime();
+
+        int rep = 100000;
+        double random = 0;
+        //ToDo разогрев
+        for (int i = 0; i < rep; i++) {
+//            if (totalCompilationTime != mxBean.getTotalCompilationTime()) {
+//                System.out.println("compiler made something" + i);
+//                totalCompilationTime = mxBean.getTotalCompilationTime();
+//            }
+            random = Math.random();
+        }
+
+        rep = 1000000;
+        //ToDo реальный замер
+        long before = System.nanoTime();
+        for (int i = 0; i < rep; i++) {
+            random = Math.random();
+        }
+        long after = System.nanoTime();
+        System.out.println(random);
+        System.out.println((after - before) / rep);
+    }
+}
