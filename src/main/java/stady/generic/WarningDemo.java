f9f67667714a2d9311d9a9205f6388271e824d3c
VasDA <D.Vasilyev@bssys.com>
2016-09-21 15:15:32 +0300

    generics

---
+++ src/main/java/stady/generic/WarningDemo.java
@@ -0,0 +1,15 @@
+package stady.generic;
+
+/**
+ * Created by VasDA on 21.09.2016.
+ */
+public class WarningDemo {
+    public static void main(String[] args) {
+        Box<Integer> bi;
+        bi = createBox();
+    }
+
+    static Box createBox() {
+        return new Box();
+    }
+}
