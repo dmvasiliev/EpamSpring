0f19460f660cd5323033ca69835343682231943c
VasDA <D.Vasilyev@bssys.com>
2016-09-23 18:03:16 +0300

    Type Inference

---
+++ src/main/java/core/generic/BoxDemo.java
@@ -0,0 +1,32 @@
+package core.generic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by VasDA on 23.09.2016.
+ */
+public class BoxDemo {
+    public static <U> void addBox(U u, List<Box<U>> boxes) {
+        Box<U> box = new Box<>();
+        box.set(u);
+        boxes.add(box);
+    }
+
+    public static <U> void outputBoxes(List<Box<U>> boxes) {
+        int counter = 0;
+        for (Box<U> box : boxes) {
+            U boxContents = box.get();
+            System.out.println("Box #" + counter + " contains [" + boxContents.toString() + "]");
+            counter++;
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Box<Integer>> listOfIntegerBoxes = new ArrayList<>();
+        BoxDemo.<Integer>addBox(Integer.valueOf(10), listOfIntegerBoxes);
+        BoxDemo.addBox(Integer.valueOf(20), listOfIntegerBoxes);
+        BoxDemo.addBox(Integer.valueOf(30), listOfIntegerBoxes);
+        BoxDemo.outputBoxes(listOfIntegerBoxes);
+    }
+}
