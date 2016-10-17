4d0fd83c52e2899779d410197b537f3ae1aa4e4f
VasDA <D.Vasilyev@bssys.com>
2016-10-17 17:41:50 +0300

    Многопоточность в Java https://habrahabr.ru/post/164487/

---
+++ src/main/java/core/collections/linkedlist/MyLinkedList.java
@@ -0,0 +1,16 @@
+package core.collections.linkedlist;
+
+import java.util.LinkedList;
+
+/**
+ * Created by VasDA on 13.10.2016.
+ */
+public class MyLinkedList {
+
+
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList();
+        linkedList.add("123");
+
+    }
+}
