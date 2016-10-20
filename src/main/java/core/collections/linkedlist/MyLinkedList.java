daedd3e7f4adb8b2e6bd56eac501a56496cb3517
VasDA <D.Vasilyev@bssys.com>
2016-10-20 21:56:45 +0300

    Многопоточность в Java https://habrahabr.ru/post/164487/

--- src/main/java/core/collections/linkedlist/MyLinkedList.java
+++ src/main/java/core/collections/linkedlist/MyLinkedList.java
@@ -1,5 +1,6 @@
 package core.collections.linkedlist;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 /**
@@ -10,7 +11,20 @@ public class MyLinkedList {
 
     public static void main(String[] args) {
         LinkedList<String> linkedList = new LinkedList();
-        linkedList.add("123");
+        linkedList.add("0");
+        linkedList.add("2");
+        linkedList.add(1, "1");
 
+
+        Iterator<String> iterator = linkedList.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+
+        Iterator<String> listIterator = linkedList.listIterator();
+        while (listIterator.hasNext()) {
+            System.out.println(listIterator.next());
+        }
     }
 }
