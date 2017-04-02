70b10b288e65a6e744853e18c29af8d886292da2
Dmitry <dimysik@gmail.com>
2017-04-02 03:03:20 +0300

    my task for site: sql-ex.ru

--- src/main/resources/dataForBase/postgreSQL/myTask.sql
+++ src/main/resources/dataForBase/postgreSQL/myTask.sql
@@ -30,3 +30,57 @@ SELECT
 FROM laptop
 WHERE price > 1000;
 
+-- Найдите номер модели, скорость и размер жесткого диска ПК, имеющих 12х или 24х CD и цену менее 600 долларов.
+SELECT
+  model,
+  speed,
+  hd
+FROM pc
+WHERE (cd = '12x' OR cd = '24x') AND pc.price < 600;
+SELECT
+  model,
+  speed,
+  hd
+FROM pc
+WHERE cd IN ('12x', '24x') AND pc.price < 600;
+
+--Укажите производителя и скорость портативных компьютеров с жестким диском объемом не менее 10 Гбайт
+
+SELECT DISTINCT
+  p.maker,
+  l.speed
+FROM product p, laptop l
+WHERE p.model = l.model AND l.hd >= 10;
+
+SELECT DISTINCT
+  p.maker,
+  l.speed
+FROM product p
+  JOIN laptop l ON p.model = l.model
+WHERE l.hd >= 10;
+
+-- Найдите номера моделей и цены всех продуктов (любого типа) выпущенных производителем B (латинская буква).
+
+SELECT
+  model,
+  price
+FROM pc
+WHERE model IN (SELECT model
+                FROM product
+                WHERE maker = 'B' AND type = 'PC')
+UNION
+SELECT
+  model,
+  price
+FROM laptop
+WHERE model IN (SELECT model
+                FROM product
+                WHERE maker = 'B' AND type = 'Laptop')
+UNION
+SELECT
+  model,
+  price
+FROM printer
+WHERE model IN (SELECT model
+                FROM product
+                WHERE maker = 'B' AND type = 'Printer');
